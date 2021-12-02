0b063e6584bc72f3c462d9317968390e05cafa3d
Abu Hasan <hasanabubachelor@gmail.com>
2021-12-02 11:22:37 +0600

    add image & todo component created

---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import todo from "../images/todo.png";
+
+const ToDo = () => {
+    return (
+        <div>
+            <div className="">
+                <figure>
+                    <img src={todo} alt=""/>
+                </figure>
+            </div>
+        </div>
+    );
+};
+
+export default ToDo;
